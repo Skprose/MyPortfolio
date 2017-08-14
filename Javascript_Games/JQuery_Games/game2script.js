60add72b60d9b85259ac7db12da6f42132f6d1a0
Skprose <sangeethakperumal@gmail.com>
2017-08-14 13:23:00 -0600

    attack power

--- Javascript_Games/JQuery_Games/game2script.js
+++ Javascript_Games/JQuery_Games/game2script.js
@@ -14,6 +14,9 @@ $(document).ready(function(){
 
           var mychar = false;
           var mypower = 0;
+          var opphealth = 0;
+          var oppattack = 0;
+          var myhealth = 0;
 
           $("#htchar1").on("click",function()
            {
@@ -24,11 +27,14 @@ $(document).ready(function(){
                        $('#characters').prop("disabled",true);
                        $("#ohealth").html("Opponent Health : "+ ar_health[0]);
                        $("#opower").html("Opponent Attach Power : "+ att_power[0]);
+                       opphealth = ar_health[0];
+                       oppattack = att_power[0];
              }
              else {
                         $("#yourcharacter").attr("src",gl_imgarray[0]);
                         mychar = true;
                         mypower = att_power[0];
+                        myhealth = ar_health[0];
                         $("#ypower").html("Your Attach Power : "+ att_power[0]);
                         $("#yhealth").html("Your Health : "+ ar_health[0]);
                }
@@ -43,11 +49,14 @@ $(document).ready(function(){
                         $('#characters').prop("disabled",true);
                         $("#ohealth").html("Opponent Health : "+ ar_health[1]);
                         $("#opower").html("Opponent Attach Power : "+ att_power[1]);
+                        opphealth = ar_health[1];
+                        oppattack = att_power[1];
               }
               else {
                          $("#yourcharacter").attr("src",gl_imgarray[1]);
                          mychar = true;
                          mypower = att_power[1];
+                         myhealth = ar_health[1];
                          $("#ypower").html("Your Attach Power : "+ att_power[1]);
                          $("#yhealth").html("Your Health : "+ ar_health[1]);;
                 }
@@ -62,11 +71,14 @@ $(document).ready(function(){
                           $('#characters').prop("disabled",true);
                           $("#ohealth").html("Opponent Health : "+ ar_health[2]);
                           $("#opower").html("Opponent Attach Power : "+ att_power[2]);
+                          opphealth = ar_health[2];
+                          oppattack = att_power[2];
                }
                else {
                           $("#yourcharacter").attr("src",gl_imgarray[2]);
                           mychar = true;
                           mypower = att_power[2];
+                          myhealth = ar_health[2];
                           $("#ypower").html("Your Attach Power : "+att_power[2]);
                           $("#yhealth").html("Your Health : "+ ar_health[2]);;
                  }
@@ -82,11 +94,14 @@ $(document).ready(function(){
                           $('#characters').prop("disabled",true);
                           $("#ohealth").html("Opponent Health : "+ ar_health[3]);
                           $("#opower").html("Opponent Attach Power : "+ att_power[3]);
+                          opphealth = ar_health[3];
+                          oppattack = att_power[3];
                 }
                 else {
                            $("#yourcharacter").attr("src",gl_imgarray[3]);
                            mychar = true;
                            mypower = att_power[3];
+                           myhealth = ar_health[3];
                            $("#ypower").html("Your Attach Power : "+ att_power[3]);
                           $("#yhealth").html("Your Health : "+ ar_health[3]);;
                   }
@@ -102,11 +117,14 @@ $(document).ready(function(){
                           $('#characters').prop("disabled",true);
                           $("#ohealth").html("Opponent Health : "+ ar_health[4]);
                          $("#opower").html("Opponent Attach Power : "+ att_power[4]);
+                         opphealth = ar_health[4];
+                         oppattack = att_power[4];
                  }
                  else {
                           $("#yourcharacter").attr("src",gl_imgarray[4]);
                           mychar = true;
                           mypower = att_power[4];
+                          myhealth = ar_health[4];
                          $("#yhealth").html("Your Health : "+ ar_health[4]);
                           $("#ypower").html("Your Attach Power : "+ att_power[4]);
                  }
@@ -121,19 +139,51 @@ $(document).ready(function(){
                           $('#characters').prop("disabled",true);
                           $("#ohealth").html("Opponent Health : "+ ar_health[5]);
                          $("#opower").html("Opponent Attach Power : "+ att_power[5]);
+                         opphealth = ar_health[5];
+                         oppattack = att_power[5];
                   }
                   else {
                            $("#yourcharacter").attr("src",gl_imgarray[5]);
                            mychar = true;
                            mypower = att_power[5];
+                           myhealth = ar_health[5];
                            $("#yhealth").html("Your Health : "+ ar_health[5]);
                            $("#ypower").html("Your Attach Power : "+ att_power[5]);
 
                   }
                 })
 
-              $("#attack").click(function(){
+              $("#attack").click(function()
+              {
+                        var att;
+                        if (oppattack > mypower)
+                        {
+                               att = oppattack - mypower;
+                               myhealth = myhealth - (att*2);
+                               opphealth = opphealth - att;
+                               mypower = mypower + parseInt(oppattack * (25 /100));
+                        }
+                        else {
+                               att = mypower - oppattack;
+                               myhealth = myhealth - att;
+                               opphealth = opphealth - (att * 2);
+                               mypower = mypower + parseInt(oppattack * (25 /100));
+                        }
 
+                        $("#yhealth").html("Your Health : "+ myhealth);
+                        $("#ypower").html("Your Attach Power : "+ mypower);
+                        $("#ohealth").html("Opponent Health : "+ opphealth);
+                        $("#opower").html("Opponent Attach Power : "+ oppattack);
+
+                        if (( myhealth > 0) && (myhealth > opphealth) && (opphealth < 1))
+                        {
+                              $("#result").html('You Win!!!')
+
+                        }
+                        elseif (( myhealth < 1) && (myhealth < opphealth) && (opphealth > 0))
+                        {
+                              $("#result").html('You Win!!!')
+                        }
               })
 
 });
