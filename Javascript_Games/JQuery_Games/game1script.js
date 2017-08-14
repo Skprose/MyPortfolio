2718d159f78e6324210fa1d315247237b1a41876
Skprose <sangeethakperumal@gmail.com>
2017-08-13 21:26:20 -0600

    jquery games update 1

---
+++ Javascript_Games/JQuery_Games/game1script.js
@@ -0,0 +1,93 @@
+
+
+ $(document).ready(function()
+{
+      console.log('In Java script');
+      var totalvalue = 0;
+      var gem1 = 0;
+      var gem2 = 0;
+      var gem3 = 0;
+      var gem4 = 0;
+      var score = 0;
+      var win = 0;
+      var loss = 0;
+      var round = 0;
+
+      nextround();
+
+
+                 $("#htgem1").on("click",function(){
+                   $("#htscore").append(gem1 + " +   ");
+                   score = score + gem1;
+                    $("#httot").html(score);
+                    console.log('score '+ score);
+                    checkscore();
+                 })
+                 $("#htgem2").on("click",function(){
+                   console.log('inside');
+                   $("#htscore").append(gem2 + " + ");
+                   score = score + gem2;
+                    $("#httot").html(score);
+                    console.log('score '+ score);
+                    checkscore();
+                 })
+                 $("#htgem3").on("click",function(){
+                   $("#htscore").append(gem3 + " + ");
+                   score = score + gem3;
+                    $("#httot").html(score);
+                    console.log('score '+ score);
+                    checkscore();
+                 })
+                 $("#htgem4").on("click",function(){
+                   $("#htscore").append(gem4 + " + ");
+                   score = score + gem4;
+                    $("#httot").html(score);
+                    console.log('score '+ score);
+                    checkscore();
+               })
+
+
+       function checkscore()
+       {
+           if ( score === totalvalue )
+           {
+                 win++;
+                 $("#htwin").html(win);
+                   console.log('Score is equal value')
+                   nextround();
+           }
+           else if ( score > totalvalue)
+           {
+                 loss++
+                 console.log('loss :'+loss);
+                 $("#htloss").html('Losses : '+loss);
+                 var diff = score - totalvalue;
+                 $("#htcomment").html("You collected Gem Stone worth "+diff+ " more!!!");
+                 nextround();
+             }
+            else {
+                  var diff = totalvalue - score;
+                  $("#htcomment").html("You need to collect Gem Stone worth "+diff+ " more!!!");
+            }
+       }
+
+       function nextround()
+       {
+
+              score = 0;
+              gem1 = parseInt(Math.random() * 100);
+              gem2 = parseInt(Math.random() * 100);
+              gem3 = parseInt(Math.random() * 100);
+              gem4 = parseInt(Math.random() * 100);
+
+              totalvalue =(gem1*2) + (gem2 *3)+ (gem3 *3 )+ (gem4 *2);
+              round++;
+
+              $("#htround").html('<h3>Round : '+ round + '</h3>');
+              $("#httotal").html(totalvalue);
+              $("#htscore").html("  ");
+              $("#htcomment").html(" ");
+               $("#httot").html(" ");
+
+       }
+});
