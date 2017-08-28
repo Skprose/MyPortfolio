cbba085bf494fbf1ba5f4dae103d459087607775
Skprose <sangeethakperumal@gmail.com>
2017-08-28 03:22:01 -0600

    dding inputs

--- Javascript_Games/GameswithTimer/assignmentgifjs.js
+++ Javascript_Games/GameswithTimer/assignmentgifjs.js
@@ -1,9 +1,9 @@
 
 //global Variables
-var gl_items = ['Ganapati','Fire works','Stars','Programming','Peacocks','Flammmingo'];
-
+var gl_items = ['Ganapati','Fire works','Stars','Programming','Peacocks','Flamingo','Flowers','Butterfly'];
+var gl_rows = 1;
 var gl_searchfor = [];
-
+var gl_rating = "G";
 
 //dynamically add form elements
 function dynamicadd()
@@ -13,7 +13,7 @@ function dynamicadd()
 
           var  additem = (`<input id = "${i}option"
           class = "optionvalue" name ="name${i}"
-          type = "radio"
+          type = "checkbox"
           value = "${gl_items[i]}" > ${gl_items[i]}<br />`);
           $("#First-form").append(additem);
         }
@@ -28,23 +28,33 @@ $("#getit").on("click",function(event){
            gl_items[l]= inputtext;
            var  additem = (`<input id = "${l}option"
            class = "optionvalue" name ="name${l}"
-           type = "radio"
+           type = "checkbox"
            value = "${gl_items[l]}" > ${gl_items[l]}<br />`);
            //divele.append(string);
              $("#First-form").append(additem);
              $("#search").val("") ;
 });
+$("#reset").on("click",function(){
+        for ( var i =0 ; i< gl_items.length; i++)
+        {
+          $(`#${i}option`).attr("checked",false);
+        }
+        $("#vfiles").html(' ');
+});
+$("#rating").on("change",function(){
+       gl_rating = this.value;
+});
 function callapigifvid(searchstr)
 {
       //Api call
           var querstr1 = "http://api.giphy.com/v1/gifs/search?api_key=01f8ebd24dd04b98b407dd4b30d3443e" ;
-          var noofgifs = 25;
-          var var_rating = "G"; // need to improvize having a drop down input
-            var querstr2 = "&q="+ searchstr +"&limit="+noofgifs+"&offset=0&rating=G&lang=en";
+          var noofgifs = gl_rows;
+          var var_rating = gl_rating; //"G"; // need to improvize having a drop down input
+            var querstr2 = "&q="+ searchstr +"&limit="+noofgifs+"&offset=0&rating="+gl_rating+"&lang=en";
             //var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=01f8ebd24dd04b98b407dd4b30d3443e&q=stars&limit=25&offset=0&rating=G&lang=en";
             var queryURL = querstr1 + querstr2 ;
             console.log(querstr1+querstr2);
-            $("#vfiles").html("");
+            //$("#vfiles").html("");
 
             $.ajax({
               url : queryURL ,
@@ -66,7 +76,7 @@ function callapigifvid(searchstr)
                 ifrele.attr("image-file",in2);
                 ifrele.attr("status","movie");
                 ifrele.attr("src",in1);
-                $("#vfiles").append(ifrele);
+                $("#vfiles").prepend(ifrele);
               } //end of for
             }).error(function(errmsg){
               console.log('Encountered Error: ',errmsg);
@@ -79,18 +89,17 @@ $("#First-form").on("click","input",function(){
     {
       gl_searchfor = this.value ;
       callapigifvid(gl_searchfor);
-
+      //callapi(gl_searchfor);
     }
     else {
-          gl_searchfor = gl_searchfor+"+"+this.value ;
+          gl_searchfor = this.value ;
           console.log('search str',gl_searchfor);
           callapigifvid(gl_searchfor);
+          //callapi(gl_searchfor);
     }
 
 });
-//$("#iputtext").on("change",function(event){
 
-//})
 $("#vfiles").on("click","img",function(event)
 {
           event.preventDefault();
@@ -110,13 +119,18 @@ $("#vfiles").on("click","img",function(event)
 
           }
 });
+$("#Second-form").on("click","input",function(event){
+          //event.preventDefault();
+          gl_rows = this.value ;
+})
 
 
 //Main Programming
 dynamicadd();
 
-/*
-function callapigifvid(searchstr)
+
+//Not used
+function callapi(searchstr)
 {
       //Api call
           var querstr1 = "http://api.giphy.com/v1/gifs/search?api_key=01f8ebd24dd04b98b407dd4b30d3443e" ;
@@ -146,12 +160,12 @@ function callapigifvid(searchstr)
                 ifrele.attr("image-file",in2);
                 ifrele.attr("status","movie");
                 ifrele.attr("src",in1);
-                $("#vfiles").append(ifrele);
+                $("#vidfiles").append(ifrele);
               } //end of for
             }).error(function(errmsg){
               console.log('Encountered Error: ',errmsg);
             });//end of ajax response
-}*/
+}
 // in the above i used iframe, so when i change the src(url) to still or moving, it opens the giphy webpage
 
 /*
@@ -169,3 +183,6 @@ this.attr("src",gl_imgurl[ind1]); */
             //var str = '<video><source src ="'+ in1+'" ></video>';
             //console.log('string'+ str);
             //console.log('in2'+in2);
+            //$("#iputtext").on("change",function(event){
+
+            //})
