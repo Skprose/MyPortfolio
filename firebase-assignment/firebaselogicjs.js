7dbdeb115ed5a3f6ec358666d634abd20f51e350
Skprose <sangeethakperumal@gmail.com>
2017-09-04 11:58:25 -0600

    adding next train next

--- firebase-assignment/firebaselogicjs.js
+++ firebase-assignment/firebaselogicjs.js
@@ -4,43 +4,10 @@ console.log('inside javascript');
 
         var database = firebase.database();
         var gl_counter = 0;
-//Display values on Tables
-        database.ref().on("child_added",function(snapshot)
-          {
-            console.log('Inside database ref call');
-            console.log(snapshot.val());
-            gl_counter++ ;
-            var lenofstr =  snapshot.val();
-            console.log('global counter :'+gl_counter);
-            var tname = snapshot.val().trainname;
-            var tdest = snapshot.val().traindestination;
-            var ttimehour = snapshot.val().firsttrainhour;
-            var ttimeminu = snapshot.val().firsttrainmin;
-            var ttimeampm = snapshot.val().firsttrainampm;
-            var ttime = ttimehour +":"+ttimeminu +":"+ttimeampm ;
-            var tfreq = snapshot.val().frequency;
-            var tkey = snapshot.val().key;
-            var insertstr =
-            `<tr class = 'row${gl_counter}' class = "tr_rec"'><td  class = 'row${gl_counter}'>${tname}
-            </td><td  class = 'row${gl_counter}'>${tdest}
-            </td><td class = 'row${gl_counter}'>${ttime}
-            </td><td class = 'row${gl_counter}'>${tfreq}
-            </td><td  class = 'row${gl_counter}'>${tfreq}
-            </td><td><button class = 'row${gl_counter}'  class = "btn btn-warning btn-md" class = "update-button">Update</button>
-            </td><td><button class = 'row${gl_counter}' class = "btn btn-danger btn-md" class = "delete-button">Delete</button>
-            </td></tr>`;
-            console.log('the adding item is insertstr',insertstr);
-            $("#listoftrainschedule").append(insertstr);
-          });
-//Looping thru to get the key values
-
-//Next Train Calculation
-            function nxttrain()
-            {
-              var now =  moment();
-              console.log('Now is: '+now);
-
-            }
+        var gl_hours = "5";
+        var gl_ampm = "am";
+        var gl_freq = "10";
+        var gl_mins = "15";
 //input validation
         function inputvalidation()
         {
@@ -56,43 +23,204 @@ console.log('inside javascript');
 //Clear field value
         function clearall()
         {
+
           $("#input-train-name").val("");
           $("#input-destination").val("");
-          $("#input-train-time-hours").value = "1";
-          $("#input-train-time-minutes").val("");
-          $("#ampm").value("am");
-          $("#input-train-frequency").value = "5";
+          $("#input-train-time-hours").value = gl_hours;
+          $("#input-train-time-minutes").value = gl_mins;
+          $("#ampm").value = gl_ampm ;
+          $("#input-train-frequency").value = gl_freq;
+
         }
 // Adding Train Details
         $("#submit-button").on("click",function()
         {
           event.preventDefault();
-          console.log("inside submit button");
           var name = $("#input-train-name").val().trim();
           var destination = $("#input-destination").val().trim();
-          var firsttrainhour = $("#input-train-time-hours").value;
-          var firsttrainmin = $("#input-train-time-minutes").val().trim();
-          var firsttrainampm = $("#ampm").value;
-          var tfreq = $("#input-train-frequency").value;
-          //inputvalidation(); //Incomplete need ot update
+          var firsttrainhour = gl_hours;
+          var firsttrainmin = gl_mins;
+          var firsttrainampm = gl_ampm;
+          var tfreq = gl_freq ;
+          //add to database
           database.ref().push({
               trainname: name,
               traindestination: destination,
               firsttrainhour : firsttrainhour,
               firsttrainmin : firsttrainmin,
               firsttrainampm : firsttrainampm,
-               frequency : tfreq,
+              frequency : tfreq,
               dateadded: firebase.database.ServerValue.TIMESTAMP
             });
             clearall();
-        });
-  // for the update button
+          });
+// changes in inpu
+$("#input-train-frequency").on("change",function()
+{
+    console.log("inside input change");
+    gl_freq = this.value;
+      console.log('frequency'+gl_freq);
+    var str = nxttrain(gl_hours,gl_mins,gl_freq);
+    console.log('this is string',str);
+    $("#disp-next-train").text(str);
+});
+$("#input-train-time-hours").on("change",function(){
+  gl_hours = this.value;
+  console.log('gl hour'+gl_hours);
+});
 
-  $(".delete-button").on("click",function(){
+$("#ampm").on("change",function(){
+  gl_ampm = this.value;
+  console.log("ampm"+gl_ampm);
+});
 
-  });
+$("#input-train-time-minutes").on("change",function(){
+  gl_mins = this.value;
+  console.log('gl mins'+gl_mins);
+});
+
+// next train
+function nxttrain(hours,mins,freq)
+{
+                //now values
+                  var now =  moment().format('LLLL');
+                  var vyear = moment().get('year');
+                  var vdate = moment().get('date');
+                  var vhour = moment().get('hour');
+                  var vminute = moment().get('minute');
+                  var vsecond = moment().get('second');
+                   console.log('year',vyear)  ;
+                   console.log('date',vdate)  ;
+                   console.log('hour',vhour)  ;
+                   console.log('mins',vminute)  ;
+                   console.log('secs',vsecond)  ;
+                   console.log('year',vyear)  ;
+
+                //rows - data values
+                  var tmintoadd = freq;
+                  var stmins = mins;
+                  var sthours = hours;
+                  console.log("to add",tmintoadd);
+                  //counting
+                  var temphour = hours;
+                  var tempmins = mins;
+                  var tempampm;
+                  while( (temphour <= sthours) && (tempmins <= stmins))
+                  {
+                    tempmins = tempmins + tmintoadd;
+                    if ( tempmins > 59)
+                    {
+                      temphour++;
+                      tempmins = tempmins - 60;
+                    }
+                  }
+                  if ( temphour > 12)
+                  {
+                    temphour = temphour - 12;
+                    tempampm = "pm";
+                  }
+                  else {
+                    tempampm = "am";
+                  }
+                return(temphour+":"+tempmins+tempampm);
+}
 
 // for delete button
+
+  $(".delete-button").on("click",function()
+  {
+          event.preventDefault();
+          var name = $("#input-train-name").val().trim();
+          var destination = $("#input-destination").val().trim();
+          var firsttrainhour = gl_hours;
+          var firsttrainmin = gl_mins;
+          var firsttrainampm = gl_ampm;
+          var tfreq = gl_freq ;
+
+         database.ref().del({
+             trainname: name,
+             traindestination: destination,
+             firsttrainhour : firsttrainhour,
+             firsttrainmin : firsttrainmin,
+             firsttrainampm : firsttrainampm,
+             frequency : tfreq,
+
+           });
+  });
+
+
+
   $(".update-button").on("click",function(){
 
-});
+   });
+
+//Child add - does not give key - display all rows from the database
+          database.ref().on("child_added",function(snapshot)
+            {
+              console.log('Inside database ref call');
+              console.log(snapshot.val());
+              gl_counter++ ;
+              var lenofstr =  snapshot.val();
+              console.log('global counter :'+gl_counter);
+              var tname = snapshot.val().trainname;
+              var tdest = snapshot.val().traindestination;
+              var ttimehour = snapshot.val().firsttrainhour;
+              var ttimeminu = snapshot.val().firsttrainmin;
+              var ttimeampm = snapshot.val().firsttrainampm;
+              var ttime = ttimehour +":"+ttimeminu +":"+ttimeampm ;
+              var tfreq = snapshot.val().frequency;
+              var tkey = snapshot.val().key;
+              console.log('key',tkey);
+              var str = nxttrain();
+              var insertstr =
+              `<tr uid = 'row${gl_counter}' class = "tr_rec"'><td  class = 'row${gl_counter}'>${tname}
+              </td><td  uid = 'row${gl_counter}'>${tdest}
+              </td><td uid = 'row${gl_counter}'>${ttime}
+              </td><td uid = 'row${gl_counter}'>${tfreq}
+              </td><td  uid = 'row${gl_counter}'>${tfreq}
+              </td><td><button uid = 'row${gl_counter}'  class = "btn btn-warning btn-md" class = "update-button">Update</button>
+              </td><td><button uid = 'row${gl_counter}' class = "btn btn-danger btn-md" class = "delete-button">Delete</button>
+              </td></tr>`;
+              console.log('e adding item is insertstr',insertstr);
+              $("#listoftrainschedule").append(insertstr);
+            });
+
+//Child deleted
+            /* database.ref().on("child_removed",function(snapshot)
+              {
+
+              })*/
+              //Display values on Tables
+              /*
+                      database.ref().on("child",function(snapshot)
+                        {
+                          console.log('Inside database ref call');
+                          console.log(snapshot.val());
+                          var content = snapshot.val();
+                          var noofrows = content.length;
+                          var uniqueid = content.keys;
+                          gl_counter++ ;
+                          for ( var key in content)
+                          {
+                                  console.log('global counter :'+gl_counter);
+                                  var tname = content[key].trainname;
+                                  var tdest = content[key].traindestination;
+                                  var ttimehour = content[key].firsttrainhour;
+                                  var ttimeminu = content[key].firsttrainmin;
+                                  var ttimeampm = content[key].firsttrainampm;
+                                  var ttime = ttimehour +":"+ttimeminu +":"+ttimeampm ;
+                                  var tfreq = content[key].frequency;
+                                  var tkey = content[key].key;
+                                  var insertstr =
+                                  `<tr class = 'row${gl_counter}' class = "tr_rec"'><td  class = 'row${gl_counter}'>${tname}
+                                  </td><td  class = 'row${gl_counter}'>${tdest}
+                                  </td><td class = 'row${gl_counter}'>${ttime}
+                                  </td><td class = 'row${gl_counter}'>${tfreq}
+                                  </td><td  class = 'row${gl_counter}'>${tfreq}
+                                  </td><td><button class = 'row${gl_counter}'  class = "btn btn-warning btn-md" class = "update-button">Update</button>
+                                  </td><td><button class = 'row${gl_counter}' class = "btn btn-danger btn-md" class = "delete-button">Delete</button>
+                                  </td></tr>`;
+                                  console.log('the adding item is insertstr',insertstr);
+                                  $("#listoftrainschedule").append(insertstr);
+                          }
+                        });*/
