20c98deec0c8b08c82238b46478c3e7385a784b1
Skprose <sangeethakperumal@gmail.com>
2017-09-29 08:22:53 -0600

    Changes in layout and adding basic file for node

---
+++ Node_assignment/node_assignment_1.js
@@ -0,0 +1,121 @@
+/* Assignment:LIRI Bot - Week:10 */
+/* Main Program - to validate the input parameters and call the respective application */
+var inputlen = process.argv.length ;
+if (inputlen <3)
+{
+  console.log("Only 2");
+}
+else {
+  if ( process.argv[2] === 'tweet')
+  {
+    getalltweets();
+
+  }
+}
+console.log("tweet");
+getalltweets();
+//gettweets();
+//console.log("movie");
+//getmoviereview();
+console.log("music");
+//getmusic();
+
+function getmusic()
+{
+
+
+        var spotify = require('node-spotify-api');
+
+        var myspotify = new spotify({
+          id:'b90fdab5d5e6450492107005d0a15337' ,
+          secret: '6e85c4889116480ea9ac113f975ce390',
+        });
+        myspotify.search({type: 'track',query:'I Want it that way',limit:2},function(err,data){
+           if(err)
+           {
+             console.log(err,'Error encountered');
+           }
+          // console.log(JSON.stringify(data,null,2));
+          console.log("Details of Spotify");
+          console.log(JSON.parse(data).external_urls);
+          console.log(JSON.parse(data).name);
+          console.log(JSON.parse(data).type);
+        });
+}
+
+/*
+    })
+    .catch(dunction(err){
+
+    })
+*/
+
+
+
+function getalltweets()
+{
+      var Twitter = require('twitter');
+        var mytwitter = new Twitter({
+            consumer_key : 'PkFDY5GnPIjXUaK9lrbfywgjp',
+            consumer_secret : 'NgQP0ZRR50XFoQDOp8OJ0XEnk5PfZupjaB1TZqlrEdPQyfadDh',
+            access_token_key : '910975361038479360-U9ZTDYMkZyEWTvAnObCBKWNDAOGyQHB',
+            access_token_secret : 'eoiAJtJJsVJHDOv18yjfcosEgHjxce9NLco2P8frLGUCX'
+        });
+
+        var params = {screen_name: 'nodejs'};
+      mytwitter.get('statuses/user_timeline',  function(error, tweets, response) {
+        if (!error && response.statusCode === 200) {
+             console.log("The tweets");
+            // console.log(tweets);
+            // console.log(JSON.parse(tweets));
+             //console.log(JSON.parse(tweets).text);
+             for (var i  =0 ; i < tweets.length ; i++)
+             {
+               console.log(tweets[i].created_at);
+               console.log(tweets[i].text);
+             }
+           }
+        if (error) throw error ;
+      });
+}
+
+function getmoviereview()
+{
+        var omdb = require("request");
+        var moviename = process.argv[2];
+        var queryurl = "http://www.omdbapi.com/?t="+moviename+"&y=&plot=short&apikey=40e9cece";
+        console.log(queryurl);
+        omdb(queryurl, function(error, response, body) {
+          if (!error && response.statusCode === 200)
+          {
+             //console.log(body);
+              console.log(JSON.parse(body).imdbRating);
+              console.log(JSON.parse(body).Title);
+              console.log(JSON.parse(body).Director);
+              console.log(JSON.parse(body).Writer);
+          }
+       });
+};
+function gettweets()
+{
+         var Twitter = require('twitter');
+          var mytwitter = new Twitter({
+              consumer_key : 'PkFDY5GnPIjXUaK9lrbfywgjp',
+              consumer_secret : 'NgQP0ZRR50XFoQDOp8OJ0XEnk5PfZupjaB1TZqlrEdPQyfadDh',
+              access_token_key : '910975361038479360-U9ZTDYMkZyEWTvAnObCBKWNDAOGyQHB',
+              access_token_secret : 'eoiAJtJJsVJHDOv18yjfcosEgHjxce9NLco2P8frLGUCX'
+          });
+
+
+          var params = {screen_name: 'nodejs'};
+          mytwitter.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (!error) {
+              console.log("Details of Twitter");
+              console.log(tweets);
+              // console.log(JSON.stringify(tweets,null,2));
+              //  console.log(JSON.parse(tweets).text);
+              //  console.log(JSON.parse(tweets).created_at);
+            }
+          });
+
+}
